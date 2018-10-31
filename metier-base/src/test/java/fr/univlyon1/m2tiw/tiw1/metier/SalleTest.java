b84d89000d99fcb209354260314e9deddfc77dae
Alper <alper.ekmekci@etu.univ-lyon1.fr>
2018-10-31 13:19:38 +0100

    Maj des tests pour SalleTest.java

--- metier-base/src/test/java/fr/univlyon1/m2tiw/tiw1/metier/SalleTest.java
+++ metier-base/src/test/java/fr/univlyon1/m2tiw/tiw1/metier/SalleTest.java
@@ -2,11 +2,13 @@ package fr.univlyon1.m2tiw.tiw1.metier;
 
 import fr.univlyon1.m2tiw.tiw1.metier.dao.impl.JSONSalleDAO;
 import java.io.IOException;
-import java.util.Collection;
 import java.util.List;
-import java.util.logging.Level;
 import java.util.logging.Logger;
+
 import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Before;
 import org.junit.Test;
 
 /*
@@ -23,15 +25,64 @@ public class SalleTest {
     private List<Salle> salles;
     private static final Logger LOGGER = Logger.getLogger(SalleTest.class.getName());
 
+    @Before
+    public void loadSalles() throws IOException {
+
+        salles = new JSONSalleDAO().load();
+    }
+
     @Test
-    public void getSalles() {
-        try {
-            salles = new JSONSalleDAO().load();
-            LOGGER.info(salles.toString());
-            assertEquals("[{nom:Salle 1, capacite:100}, {nom:Salle 2, capacite:70}, "
-                    + "{nom:Salle 3, capacite:50}]",salles.toString());
-        } catch (IOException ex) {
-            Logger.getLogger(SalleTest.class.getName()).log(Level.SEVERE, null, ex);
+    public void getSalles() throws IOException {
+        //salles = new JSONSalleDAO().load();
+        LOGGER.info(salles.toString());
+        assertEquals("[{nom:Salle 1, capacite:100}, {nom:Salle 2, capacite:70}, "
+                + "{nom:Salle 3, capacite:50}]", salles.toString());
+    }
+
+    @Test
+    public void getNbSalles() throws IOException {
+
+        //salles = new JSONSalleDAO().load();
+        LOGGER.info(salles.toString());
+
+        //Il y a 3 salles
+        // - {nom:Salle 1, capacite:100},
+        // - {nom:Salle 2, capacite:70},
+        // - {nom:Salle 3, capacite:50}
+        assertEquals(3, salles.size());
+    }
+
+    @Test
+    public void getNom() throws IOException {
+
+        //salles = new JSONSalleDAO().load();
+        LOGGER.info(salles.get(0).getNom());
+        LOGGER.info(salles.get(1).getNom());
+        LOGGER.info(salles.get(2).getNom());
+
+        //Il y a 3 salles
+        // - {nom:Salle 1, capacite:100},
+        // - {nom:Salle 2, capacite:70},
+        // - {nom:Salle 3, capacite:50}
+        int i = 1;
+        while(i <= salles.size()) {
+            assertEquals("Salle " +i , salles.get(i - 1).getNom());
+            i++;
         }
+    }
+
+
+    @Test
+    public void getCapacite() throws IOException {
+
+        //salles = new JSONSalleDAO().load();
+
+        //Il y a 3 salles
+        // - {nom:Salle 1, capacite:100},
+        // - {nom:Salle 2, capacite:70},
+        // - {nom:Salle 3, capacite:50}
+        assertEquals(100, salles.get(0).getCapacite());
+        assertEquals(70 , salles.get(1).getCapacite());
+        assertEquals(50, salles.get(2).getCapacite());
     }
 }
